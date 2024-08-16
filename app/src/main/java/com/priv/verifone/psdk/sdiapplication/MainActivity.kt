4ad94ec693df3d07269a2382e169ed11bb78bc80
Subham Pattanaik <subham.pattanaik@verifone.com>
2024-08-16 15:47:48 +0530

    Added peripheral integration

--- app/src/main/java/com/priv/verifone/psdk/sdiapplication/MainActivity.kt
+++ app/src/main/java/com/priv/verifone/psdk/sdiapplication/MainActivity.kt
@@ -50,7 +50,8 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_transaction, R.id.nav_updateservice, R.id.nav_config, R.id.nav_usb
+                R.id.nav_home, R.id.nav_transaction, R.id.nav_peripheral_service,
+                R.id.nav_updateservice, R.id.nav_config, R.id.nav_usb
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
